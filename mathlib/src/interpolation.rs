f921d794fc6cdb785e1dcae092e42448b0c87b0d
Glen Nelson <glen.nelson.1@gmail.com>
2025-12-01 00:06:39 -0700

    Add shoelace theory from 2023 to mathlib

--- mathlib/src/interpolation.rs
+++ mathlib/src/interpolation.rs
@@ -1,10 +1,10 @@
-use std::ops::{Add, Sub, Mul, Div};
+use std::ops::{Add, Div, Mul, Sub};
 
 /// Linear Interpolation finds percentage T between two values
 /// ```
 /// assert_eq!(mathlib::lerp(3.2, 5.8, 0.32), 4.032);
 /// ```
-pub fn lerp<T>(start: T, end: T, percentage: T) -> T 
+pub fn lerp<T>(start: T, end: T, percentage: T) -> T
 where
     T: Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Copy,
 {
@@ -15,12 +15,9 @@ where
 /// ```
 /// assert_eq!(mathlib::remap(5.4, 1.0, 20.0, -5.0, 5.0), -2.6842105263157894);
 /// ```
-pub fn remap<T>(value: T, low1: T, high1: T, low2: T, high2: T) -> T 
+pub fn remap<T>(value: T, low1: T, high1: T, low2: T, high2: T) -> T
 where
     T: Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Div<Output = T> + Copy,
 {
     low2 + (value - low1) * (high2 - low2) / (high1 - low1)
 }
-
-
-
