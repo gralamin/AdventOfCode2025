fd509edaf9cc45b5eef78306c68b060af8e528c9
Glen Nelson <glen.nelson.1@gmail.com>
2025-11-30 22:25:08 -0700

    day01: Solution to part1.
    Tricky part in rust here is the modulus, but luckily already had a function for that ready.

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -2,16 +2,51 @@ extern crate filelib;
 
 pub use filelib::load_no_blanks;
 use log::info;
+use mathlib::modulus;
 
-/// Foo
+type PuzzleInt = i32;
+
+// 0 through 99 in order
+// Right = addition
+// Left = subtraction
+// % 100 the value
+// count how many times its at 0
+
+fn convert_ints(string_list: &Vec<String>) -> Vec<PuzzleInt> {
+    return string_list
+        .iter()
+        .map(|s: &String| {
+            if s.starts_with("L") {
+                -1 * s[1..].parse::<PuzzleInt>().unwrap()
+            } else {
+                s[1..].parse::<PuzzleInt>().unwrap()
+            }
+        })
+        .collect();
+}
+
+fn move_int(value: PuzzleInt, rotation: PuzzleInt) -> PuzzleInt {
+    return modulus(value + rotation, 100);
+}
+
+/// Count how many times we arrive at 0.
 /// ```
 /// let vec1: Vec<String> = vec![
-///     "foo"
+///     "L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"
 /// ].iter().map(|s| s.to_string()).collect();
-/// assert_eq!(day01::puzzle_a(&vec1), 0);
+/// assert_eq!(day01::puzzle_a(&vec1), 3);
 /// ```
-pub fn puzzle_a(string_list: &Vec<String>) -> u32 {
-    return 0;
+pub fn puzzle_a(string_list: &Vec<String>) -> usize {
+    let input = convert_ints(string_list);
+    let mut v = 50;
+    let mut count: usize = 0;
+    for i in input {
+        v = move_int(v, i);
+        if v == 0 {
+            count += 1;
+        }
+    }
+    return count;
 }
 
 /// Foo
@@ -38,5 +73,38 @@ pub fn coverage_workaround(a: &Vec<u32>) -> u32 {
         return 1;
     } else {
         return 2;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_simple() {
+        let vec1: Vec<String> = vec![
+            "L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82",
+        ]
+        .iter()
+        .map(|s| s.to_string())
+        .collect();
+        let expected = vec![-68, -30, 48, -5, 60, -55, -1, -99, 14, -82];
+        assert_eq!(convert_ints(&vec1), expected);
+    }
+
+    #[test]
+    fn test_move_above_99() {
+        let i = 99;
+        let r = 20;
+        let expected = 19;
+        assert_eq!(move_int(i, r), expected);
+    }
+
+    #[test]
+    fn test_move_below_0() {
+        let i = 5;
+        let r = -10;
+        let expected = 95;
+        assert_eq!(move_int(i, r), expected);
     }
 }
