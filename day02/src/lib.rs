aa2b7b2c4eaf5b55e9a02af6f1f332f69e8fdd22
Glen Nelson <glen.nelson.1@gmail.com>
2025-12-01 23:20:05 -0700

    Day2 solution:
    Accidentally solved part b before part 1, whoops

--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -3,8 +3,8 @@ extern crate filelib;
 pub use filelib::load_no_blanks;
 use log::info;
 
-type RangeType = u64;
-type ResultType = u128;
+type RangeType = usize;
+type ResultType = usize;
 
 fn get_ranges(input: &String) -> Vec<(RangeType, RangeType)> {
     let mut output = vec![];
@@ -53,17 +53,7 @@ fn is_repeated_twice(number: RangeType) -> bool {
     return first_half == second_half;
 }
 
-fn is_repeated_any_number_of_times(number: RangeType) -> bool {
-    // Maybe accidentally solved problem 2 ahead of time, lol.
-    let s = number.to_string();
-    let doubled = format!("{}{}", s, s);
-    let search_area = &doubled[1..doubled.len() - 1];
-    // If the original sring exists in the shaved double, its periodic.
-    // This is called a rotation trick.
-    return search_area.contains(&s);
-}
-
-/// Find all repeated strings, add them together
+/// Find all strings repeated twice, add them together
 /// ```
 /// let vec1: Vec<String> = vec![
 ///     "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
@@ -82,15 +72,45 @@ pub fn puzzle_a(string_list: &Vec<String>) -> ResultType {
         .sum::<ResultType>();
 }
 
-/// Foo
+fn find_invalid_ids_b(first: RangeType, last: RangeType) -> Vec<ResultType> {
+    // Naive way, iterate over, returning every invalid id
+    let mut output = vec![];
+    for i in first..=last {
+        if is_repeated_any_number_of_times(i) {
+            info!("Found repeat: {}", i);
+            output.push(i.try_into().expect("u32 should fit in u64"));
+        }
+    }
+    return output;
+}
+
+fn is_repeated_any_number_of_times(number: RangeType) -> bool {
+    // Maybe accidentally solved problem 2 ahead of time, lol.
+    let s = number.to_string();
+    let doubled = format!("{}{}", s, s);
+    let search_area = &doubled[1..doubled.len() - 1];
+    // If the original sring exists in the shaved double, its periodic.
+    // This is called a rotation trick.
+    return search_area.contains(&s);
+}
+
+/// Find all strings repeated any number of times, return them together.
 /// ```
 /// let vec1: Vec<String> = vec![
-///     "foo"
+///     "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
 /// ].iter().map(|s| s.to_string()).collect();
-/// assert_eq!(day02::puzzle_b(&vec1), 0);
+/// assert_eq!(day02::puzzle_b(&vec1), 4174379265);
 /// ```
-pub fn puzzle_b(string_list: &Vec<String>) -> u32 {
-    return 0;
+pub fn puzzle_b(string_list: &Vec<String>) -> ResultType {
+    let ranges = get_ranges(string_list.first().expect("At least one line of input"));
+    return ranges
+        .iter()
+        .map(|(first, last)| {
+            find_invalid_ids_b(*first, *last)
+                .into_iter()
+                .sum::<ResultType>()
+        })
+        .sum::<ResultType>();
 }
 
 #[cfg(test)]
@@ -151,6 +171,13 @@ mod tests {
 
         result = find_invalid_ids(2121212118, 2121212124);
         expected = vec![];
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_find_invalid_ids_n_times() {
+        let result = find_invalid_ids_b(11110, 11112);
+        let expected = vec![11111];
         assert_eq!(result, expected);
     }
 }
