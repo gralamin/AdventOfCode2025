2b32b555a73a8f60759288ab412133231942cfda
Glen Nelson <glen.nelson.1@gmail.com>
2025-12-04 22:24:04 -0700

    Day05: Solution to part A
    Sort the list as a small optimization, then iterate through.

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,11 +1,12 @@
-use day05::load_no_blanks;
 use day05::puzzle_a;
 use day05::puzzle_b;
+use day05::split_lines_by_blanks;
+use day05::load;
 
 fn main() {
     colog::init();
     let filename = "input";
-    let lines = load_no_blanks(filename);
+    let lines = split_lines_by_blanks(&load(filename));
 
     let value = puzzle_a(&lines);
     println!("Answer to 1st question: {}", value);
