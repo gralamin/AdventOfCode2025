c9faf6440d675230c668111a3f4966d6a606607c
Glen Nelson <glen.nelson.1@gmail.com>
2025-12-10 20:13:03 -0700

    day11: Added template

---
+++ day11/src/main.rs
@@ -0,0 +1,15 @@
+use day11::load_no_blanks;
+use day11::puzzle_a;
+use day11::puzzle_b;
+
+fn main() {
+    colog::init();
+    let filename = "input";
+    let lines = load_no_blanks(filename);
+
+    let value = puzzle_a(&lines);
+    println!("Answer to 1st question: {}", value);
+
+    let value_b = puzzle_b(&lines);
+    println!("Answer to 2nd question: {}", value_b);
+}
